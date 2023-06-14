c7db321ab0360475a3a448165553f86f64c01d0f
simplyabc <DSLushnikov@yandex.ru>
2023-06-14 21:07:46 +0500

    add form phone number validators

--- hrumshop/cart/forms.py
+++ hrumshop/cart/forms.py
@@ -1,8 +1,21 @@
-from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, CharField, TextInput, Textarea
 from .models import Order
 
+#validators=[RegexValidator(regex = r"^\+?1?\d{8,15}$")]
 
-class OrderAddForm(forms.ModelForm):
+class OrderAddForm(ModelForm):
     class Meta:
         model = Order
         fields = ['id', 'name', 'phone', 'address', 'comments']
+        widgets = {
+            'phone': TextInput(attrs={'type': 'tel', 'pattern': '[0-9]{3}-[0-9]{2}-[0-9]{3}'}),
+            'comments': Textarea(attrs={'cols': 60, 'rows': 10})
+        }
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if len(phone) > 11:
+            raise ValidationError('Длина номера телефона превышает 11 символов')
+
+        return phone
